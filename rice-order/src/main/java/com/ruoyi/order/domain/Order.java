cb1c1ad8224c7a0044092ddbef01c3f21cf51c84
阿海 <oncwnuP9j-MqdFla7hxbpaC9K6NU@git.weixin.qq.com>
2021-11-05 23:30:16 +0800

    订单管理优化逻辑

--- rice-order/src/main/java/com/ruoyi/order/domain/Order.java
+++ rice-order/src/main/java/com/ruoyi/order/domain/Order.java
@@ -1,6 +1,11 @@
 package com.ruoyi.order.domain;
 
 import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.ruoyi.common.utils.uuid.IdUtils;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import com.ruoyi.common.annotation.Excel;
@@ -8,7 +13,7 @@ import com.ruoyi.common.core.domain.BaseEntity;
 
 /**
  * 订单对象 rice_order
- * 
+ *
  * @author panghai
  * @date 2021-11-04
  */
@@ -19,9 +24,12 @@ public class Order extends BaseEntity
     /** 订单id */
     private Long orderId;
 
-    /** 取餐号码 */
-    @Excel(name = "取餐号码")
+    /** 订单编号 */
+    @Excel(name = "订单编号")
     private String orderCode;
+
+    /** 取餐号码 */
+    private int mealNumber;
 
     /** 就餐食堂id */
     private Long canteenId;
@@ -29,78 +37,103 @@ public class Order extends BaseEntity
     /** 订单价格合计 */
     private BigDecimal orderPrice;
 
-    /** 订单状态（0待支付 1已支付 2已取消） */
-    @Excel(name = "订单状态", readConverterExp = "0=待支付,1=已支付,2=已取消")
+    /** 订单状态（0未处理 1已完成 2处理失败 3已取消） */
+    @Excel(name = "订单状态", readConverterExp = "0=未处理,1=已完成,2=处理失败,3=已取消")
     private String status;
+
+    /** 创建时间 */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Excel(name = "创建时间", width = 30, dateFormat = "yyyy-MM-dd HH:mm:ss")
+    private Date createTime;
 
     /** 删除标志（0代表存在 2代表删除） */
     private String delFlag;
 
-    public void setOrderId(Long orderId) 
-    {
+    /** 时间范围 */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private List<Date> timeList;
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
         this.orderId = orderId;
     }
 
-    public Long getOrderId() 
-    {
-        return orderId;
+    public String getOrderCode() {
+        return orderCode;
     }
-    public void setOrderCode(String orderCode) 
-    {
+
+    public void setOrderCode(String orderCode) {
         this.orderCode = orderCode;
     }
 
-    public String getOrderCode() 
-    {
-        return orderCode;
+    public int getMealNumber() {
+        return mealNumber;
     }
-    public void setCanteenId(Long canteenId) 
-    {
+
+    public void setMealNumber(int mealNumber) {
+        this.mealNumber = mealNumber;
+    }
+
+    public Long getCanteenId() {
+        return canteenId;
+    }
+
+    public void setCanteenId(Long canteenId) {
         this.canteenId = canteenId;
     }
 
-    public Long getCanteenId() 
-    {
-        return canteenId;
+    public BigDecimal getOrderPrice() {
+        return orderPrice;
     }
-    public void setOrderPrice(BigDecimal orderPrice) 
-    {
+
+    public void setOrderPrice(BigDecimal orderPrice) {
         this.orderPrice = orderPrice;
     }
 
-    public BigDecimal getOrderPrice() 
-    {
-        return orderPrice;
+    public String getStatus() {
+        return status;
     }
-    public void setStatus(String status) 
-    {
+
+    public void setStatus(String status) {
         this.status = status;
     }
 
-    public String getStatus() 
-    {
-        return status;
-    }
-    public void setDelFlag(String delFlag) 
-    {
-        this.delFlag = delFlag;
+    @Override
+    public Date getCreateTime() {
+        return createTime;
     }
 
-    public String getDelFlag() 
-    {
-        return delFlag;
+    @Override
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public List<Date> getTimeList() {
+        return timeList;
+    }
+
+    public void setTimeList(List<Date> timeList) {
+        this.timeList = timeList;
     }
 
     @Override
     public String toString() {
-        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
-            .append("orderId", getOrderId())
-            .append("orderCode", getOrderCode())
-            .append("canteenId", getCanteenId())
-            .append("orderPrice", getOrderPrice())
-            .append("status", getStatus())
-            .append("delFlag", getDelFlag())
-            .append("createTime", getCreateTime())
-            .toString();
+        return "Order{" +
+                "orderId=" + orderId +
+                ", orderCode='" + orderCode + '\'' +
+                ", mealNumber=" + mealNumber +
+                ", canteenId=" + canteenId +
+                ", orderPrice=" + orderPrice +
+                ", status='" + status + '\'' +
+                ", createTime=" + createTime +
+                ", timeList=" + timeList +
+                '}';
     }
 }
--- rice-order/src/main/java/com/ruoyi/order/service/impl/OrderServiceImpl.java
+++ rice-order/src/main/java/com/ruoyi/order/service/impl/OrderServiceImpl.java
@@ -1,7 +1,9 @@
 package com.ruoyi.order.service.impl;
 
 import java.util.List;
+
 import com.ruoyi.common.utils.DateUtils;
+import com.ruoyi.common.utils.uuid.IdUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.ruoyi.order.mapper.OrderMapper;
@@ -10,86 +12,81 @@ import com.ruoyi.order.service.IOrderService;
 
 /**
  * 订单Service业务层处理
- * 
+ *
  * @author panghai
  * @date 2021-11-04
  */
 @Service
-public class OrderServiceImpl implements IOrderService 
-{
+public class OrderServiceImpl implements IOrderService {
     @Autowired
     private OrderMapper orderMapper;
 
     /**
      * 查询订单
-     * 
+     *
      * @param orderId 订单主键
      * @return 订单
      */
     @Override
-    public Order selectOrderByOrderId(Long orderId)
-    {
+    public Order selectOrderByOrderId(Long orderId) {
         return orderMapper.selectOrderByOrderId(orderId);
     }
 
     /**
      * 查询订单列表
-     * 
+     *
      * @param order 订单
      * @return 订单
      */
     @Override
-    public List<Order> selectOrderList(Order order)
-    {
+    public List<Order> selectOrderList(Order order) {
         return orderMapper.selectOrderList(order);
     }
 
     /**
      * 新增订单
-     * 
+     *
      * @param order 订单
      * @return 结果
      */
     @Override
-    public int insertOrder(Order order)
-    {
+    public int insertOrder(Order order) {
+//        Order order1 =
         order.setCreateTime(DateUtils.getNowDate());
+        order.setOrderCode(IdUtils.fastSimpleUUID());
         return orderMapper.insertOrder(order);
     }
 
     /**
      * 修改订单
-     * 
+     *
      * @param order 订单
      * @return 结果
      */
     @Override
-    public int updateOrder(Order order)
-    {
+    public int updateOrder(Order order) {
         return orderMapper.updateOrder(order);
     }
 
     /**
      * 批量删除订单
-     * 
+     *
      * @param orderIds 需要删除的订单主键
      * @return 结果
      */
     @Override
-    public int deleteOrderByOrderIds(Long[] orderIds)
-    {
+    public int deleteOrderByOrderIds(Long[] orderIds) {
         return orderMapper.deleteOrderByOrderIds(orderIds);
     }
 
     /**
      * 删除订单信息
-     * 
+     *
      * @param orderId 订单主键
      * @return 结果
      */
     @Override
-    public int deleteOrderByOrderId(Long orderId)
-    {
+    public int deleteOrderByOrderId(Long orderId) {
         return orderMapper.deleteOrderByOrderId(orderId);
     }
 }
