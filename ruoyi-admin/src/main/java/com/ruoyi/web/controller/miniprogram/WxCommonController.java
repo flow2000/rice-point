ebddbdeef46bb7a09f0a6f6f7ef3064c8c2738bd
flow2000 <1982989137@qq.com>
2023-03-11 13:41:31 +0800

    增加用户文件上传接口

---
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/miniprogram/WxCommonController.java
@@ -0,0 +1,45 @@
+package com.ruoyi.web.controller.miniprogram;
+
+import com.ruoyi.common.config.RuoYiConfig;
+import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.utils.file.FileUploadUtils;
+import com.ruoyi.framework.config.ServerConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @Author: panghai
+ * @Date: 2023/03/11
+ * @Description: 小程序通用请求处理
+ */
+
+@RestController
+@RequestMapping("/miniprogram")
+public class WxCommonController {
+
+    @Autowired
+    private ServerConfig serverConfig;
+
+    /**
+     * 头像上传
+     */
+    @PostMapping("/file/upload")
+    public AjaxResult uploadFile(MultipartFile file) throws Exception {
+        try {
+            // 上传文件路径
+            String filePath = RuoYiConfig.getUploadPath();
+            // 上传并返回新文件名称
+            String fileName = FileUploadUtils.upload(filePath, file);
+            String url = serverConfig.getUrl() + fileName;
+            AjaxResult ajax = AjaxResult.success();
+            ajax.put("fileName", fileName);
+            ajax.put("url", url);
+            return ajax;
+        } catch (Exception e) {
+            return AjaxResult.error(e.getMessage());
+        }
+    }
+}
